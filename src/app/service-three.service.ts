3fdd5f449cfb627b61cae28d6150786cadb8bdf7
sai charan kutikuppala <kutikuppalasaicharan97@gmail.com>
2023-02-08 22:32:32 +0530

    removed root provider for service-3

--- src/app/service-three.service.ts
+++ src/app/service-three.service.ts
@@ -2,9 +2,7 @@ import { Injectable } from '@angular/core';
 
 console.warn("Service-three is bundled, even if it is not being used. It doesn't have < ProvidedIn:'root' >.")
 
-@Injectable({
-
-})
+@Injectable()
 export class ServiceThree {
   constructor() { }
 }
