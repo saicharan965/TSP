db501b98fce356c0d8df37f7c63aa5992ab9c818
sai charan <87169322+saicharan965@users.noreply.github.com>
2023-02-08 10:26:01 +0530

    Update service-three.service.ts

--- src/app/service-three.service.ts
+++ src/app/service-three.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 console.warn("Service-three is bundled, even if it is not being used. It doesn't have < ProvidedIn:'root' >.")
 
 @Injectable({
-  providedIn: 'root'
+
 })
 export class ServiceThree {
   constructor() { }
